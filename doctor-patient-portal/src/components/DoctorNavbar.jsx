370f418dce0ae687a625a97f572abc180a1d3c38
arohi-karhade <arohikarhade@gmail.com>
2025-03-25 21:26:14 +0530

    Navbar logo added

--- doctor-patient-portal/src/components/DoctorNavbar.jsx
+++ doctor-patient-portal/src/components/DoctorNavbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/DoctorNavbar.css"; // External CSS file
+import logo from "../assets/medilogo.png";
 
 function DoctorNavbar({ setShowModal, handleLogout }) {
   return (
     <nav className="navbar">
-      <div className="logo">MediPlus</div>
-      <div className="nav-links">
+      <div className="logo-container">
+        <img src={logo} alt="MediPlus Logo" className="logo-img" />
+        <span className="logo-text">MediPlus</span>
+      </div>      <div className="nav-links">
         <Link to="/" className="nav-item">Home</Link>
         <button onClick={setShowModal}>Change Password</button>
         <button onClick={handleLogout}>Logout</button>
--- doctor-patient-portal/src/components/UserNavbar.jsx
+++ doctor-patient-portal/src/components/UserNavbar.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/DoctorNavbar.css"; // External CSS file
+import logo from "../assets/medilogo.png";
 
 function UserNavbar({ setShowPasswordModal, handleLogout }) {
   return (
     <nav className="navbar">
-      <div className="logo">MediPlus</div>
+      <div className="logo-container">
+        <img src={logo} alt="MediPlus Logo" className="logo-img" />
+        <span className="logo-text">MediPlus</span>
+      </div>
       <div className="nav-links">
         <Link to="/" className="nav-item">Home</Link>
         <button onClick={setShowPasswordModal}>Change Password</button> {/* Fix */}
